66c5932213153aa86d1a4244666ab698fe8facba
matao6 <1152843670@qq.com>
2017-08-03 10:57:26 +0800

    login end

--- Admin/Home/View/login/login.js
+++ Admin/Home/View/login/login.js
@@ -4,19 +4,18 @@ $(function () {
         // $(".pwd").val(localStorage.pwd);
     } else if (localStorage.name == "") {
         $(".name").val("");
-        $(".pwd").val("");
+        // $(".pwd").val("");
     }
 })
 
 var secondC = false; // 对于已经出现了验证码框的判断值
-$("input:submit").on("click", function () {
+$(".submit").on("click", function () {
     var name = $(".name").val();
     var pwd = $(".pwd").val();
     var check = $(".check").val();
-    var stopForm = true; // 阻止表单提交的判断值
 
     if (!(/^1[34578]\d{9}$/.test(name)) || !(/^.{6,12}$/).test(pwd)) {
-        bootbox.alert("手机号码或密码输入有误，请重填");
+        bootbox.alert("手机号码输入有误，请重填");
         return false;
     }
     $.ajax({
@@ -38,17 +37,15 @@ $("input:submit").on("click", function () {
                 localStorage.setItem("pwd", "");
             }
             data.code = 3;
-            data.message = "检测到您登录地址与上次登录地址不同，请进行身份核实登录";
             if (data.code == "0") { // "账号或密码有误"
                 bootbox.alert(data.message);
-                stopForm = false;
+                return false;
             } else if (data.code == "1") { // "登录成功"
                 bootbox.alert(data.message);
                 $(".submit").val("登陆中...");
-                stopForm = true;
-            }
-            if (data.code == "3") { // "检测到您登录地址与上次登录地址不同，请进行身份核实登录"
-                if (secondC) { // 第一次点击阻止表单提交，已经显示验证码框。第二次点击时，对验证码验证
+                location.href = "?login-success";
+            } else if (data.code == "3") { // "检测到您登录地址与上次登录地址不同，请进行身份核实登录"
+                if (secondC) { // 第一次点击阻止ajax提交，已经显示验证码框。第二次点击时，对验证码验证
                     $.ajax({
                         url: "http://fenxiao.qphvip.com/admin.php/Login/CheckLogin",
                         type: "POST",
@@ -62,11 +59,10 @@ $("input:submit").on("click", function () {
                         success: function (data) {
                             if (data.code == "0") {
                                 bootbox.alert(data.message);
-                                stopForm = false;
+                                return false;
                             } else if (data.code == "1") {
                                 bootbox.alert(data.message);
                                 $(".submit").val("登陆中...");
-                                stopForm = true;
                             }
                         }
                     })
@@ -76,67 +72,70 @@ $("input:submit").on("click", function () {
                     // $(".request").countDown();
                     countDown();
                     secondC = true;
-                    stopForm = false;
+                    return false;
                 }
             }
         }
     });
-    return stopForm;
 })
 
-// $.fn.extend({
-//     countDown: function () {
-//         if ($(this).setTime) {
-//             if (time == 0) {
-//                 $(this).text("获取验证码");
-//                 clearInterval(set);
-//             } else {
-//                 $(this).text(time + 's');
-//                 time--;
-//                 var set = setTimeout(function () {
-//                     $(this).countDown();
-//                 }, 1000);
-//             }
-//         } else {
-//             $(this).setTime = 60;
-//         }
-//     }
-// });
-
 var time = 60;
+
 function countDown() {
     if (time == 0) {
-        $(".request").text("获取验证码");
+        $(".request").val("获取验证码");
+        $(".request").attr("disabled", false);
+        $(".request").css("cursor", "pointer");
         time = 60;
         clearInterval(set);
     } else {
-        $(".request").text(time+'s');
+        $(".request").val(time + 's');
+        $(".request").attr("disabled", true);
+        $(".request").css("cursor", "default");
         time--;
-        var set = setTimeout(function(){
+        var set = setTimeout(function () {
             countDown()
         }, 1000);
     }
 }
 
-/* $.ajax({  
-        url : 'http://fenxiao.qphvip.com/admin.php/Home/Index/PostLogin',  
-        data : {  
-            P:name,
-            M:pwd
-        },  
-        dataType : 'json',  
-        success : function(data) {  
-            console.log(data);
-            if (data.code == "0") {  
-                alert("账号或密码错误");  
-            } else if (data.code=="1") {
-                alert("登录成功");
-            }else if (data.code=="2") {
-                alert("该账号已禁用，请联系总管理员");
-            }else if (data.code=="3") {
-                alert("检测到您登录地址与上次登录地址不同，请进行身份核实验证");
-
+$(".request").click(function () {
+    var name = $(".name").val();
+    $.ajax({
+        url: "http://fenxiao.qphvip.com/admin.php/Login/SendCode",
+        type: "POST",
+        data: {
+            P: name
+        },
+        dataType: "json",
+        success: function (data) {
+            countDown();
+            // data.code=1;
+            if (data.code == "0") {
+                bootbox.alert(data.message);
+                return false;
+            } else if (data.code == "1") {
+                bootbox.alert(data.message);
             }
+        }
+    })
+})
 
-        } 
-});  */
+// $.fn.extend({
+//     countDown: function () {
+//         if ($(this).prop(setTime)) {
+//             if ($(this).attr(setTime) == 0) {
+//                 $(this).text("获取验证码");
+//                 clearInterval(set);
+//             } else {
+//                 $(this).text(time + 's');
+//                 $(this).attr(setTime)--;
+//             }
+//         } else {
+//             $(this).attr(setTime) = 60;
+//         }
+//         var set = setTimeout(function () {
+//             $(this).countDown();
+//         }, 1000);
+//     }
+// });
